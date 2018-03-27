8f0fd794a1eb4529eb4f1f9a7cf26c87ff006ec1
Cuiqing Li <lixx3527@umn.edu>
2018-03-26 21:26:22 -0400

    finish coding part

--- project 3/gameoflife.c
+++ project 3/gameoflife.c
@@ -48,6 +48,61 @@ void update_sub_grid(int id,int num_of_pro_size,int sub_grid[],
 
 }
 
+int* get_neighbors(int i,int dimensional_size){
+    int* neig = (int*)malloc(sizeof(int)*8);
+
+    int row = i/DIM;
+    int col = i%DIM;
+    neig[0] = ((row-1+DIM)%DIM)*DIM + (col-1+DIM)%DIM;
+    neig[1] = ((row-1+DIM)%DIM)*DIM + col;
+    neig[2] = ((row-1+DIM)%DIM)*DIM + (col+1)%DIM;
+    neig[3] = row*DIM + (col-1+DIM)%DIM;
+    //neig[4] = i;
+    neig[4] = row*DIM + (col+1)%DIM;
+    neig[5] = ((row+1)%DIM)*DIM + (col-1+DIM)%DIM;
+    neig[6] = ((row+1)%DIM)*DIM + col;
+    neig[7] = ((row+1)%DIM)*DIM + (col+1)%DIM;
+
+    return neig;
+}
+
+int* get_new_subgrid(int* sub_grid,int num_of_pro_size,int dimensional_size){
+    int* new_sub_grid = (int*)malloc(sizeof(int)*(num_of_pro_size-2*dimensional_size));
+
+
+    int p = 0;
+    for(int i=dimensional_size;i<num_of_pro_size-dimensional_size;i++){
+        int* neig = get_neighbors(i,dimensional_size);
+        int count = 0;
+        for(int j=0;j<8;j++){
+        	count += sub_grid[*(neig+j)];
+        }
+
+        if(sub_grid[i]==1){
+            if(count==2 || count==3){
+               new_sub_grid[p] = 1;
+            }else{
+            	new_sub_grid[p] = 0;
+            }
+        }else{
+
+        	if(count==3){
+        		new_sub_grid[p] = 1;
+        	}else{
+        		new_sub_grid[p] = 0;
+        	}
+
+        }
+        p++;
+    }
+
+    for(int i=0;i<num_of_pro_size-2*dimensional_size;i++){
+    	sub_grid[i+dimensional_size] = new_sub_grid[i];
+    }
+
+    return new_sub_grid;
+}
+
 int main ( int argc, char** argv ) {
 
 	  int global_grid[ 256 ] = 
@@ -102,37 +157,46 @@ int main ( int argc, char** argv ) {
 	  assert ( DIM % num_procs == 0 );
 
 	  // TODO Setup your environment as necessary
-      int num_of_pro_size = GRID_WIDTH/num_procs;
-      int* updated_grid = NULL;
+      int num_of_pro_size = (DIM/num_procs+2)*DIM;
+      //int* updated_grid = NULL;
       int* sub_grid = (int*)malloc(sizeof(int)*num_of_pro_size);
+
+
+      MPI_Scatter(global_grid,GRID_WIDTH/num_procs,MPI_INT,
+      	&sub_grid[DIM],GRID_WIDTH/num_procs,MPI_INT,0,MPI_COMM_WORLD);
 
 
 	  for ( iters = 0; iters < num_iterations; iters++ ) {
 	    // TODO: Add Code here or a function call to you MPI code
-        MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Scatter(global_grid,num_of_pro_size,MPI_INT,sub_grid,
-        	num_of_pro_size,MPI_INT,0,MPI_COMM_WORLD);
 
-        update_sub_grid(ID,num_of_pro_size,
-        	sub_grid,global_grid,MPI_COMM_WORLD);
+	    //MPI_Barrier(MPI_COMM_WORLD);
+        int previous_layer = (ID==0)?(num_procs-1):ID-1;
+        int next_layer = (ID==num_procs-1)?0:ID+1;
 
-        MPI_Barrier(MPI_COMM_WORLD);
+	    if(ID%2==0){
+             MPI_Send(sub_grid+DIM,DIM,MPI_INT,previous_layer,0,MPI_COMM_WORLD);
+             MPI_Send(sub_grid+num_of_pro_size-2*DIM,DIM,MPI_INT,next_layer,0,MPI_COMM_WORLD);
 
-        if(ID==0){
-         	updated_grid=(int *)malloc(sizeof(int)*GRID_WIDTH);
-         	assert(updated_grid!=NULL);
-        }
+             MPI_Recv(sub_grid,DIM,MPI_INT,previous_layer,0,
+             	MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+             MPI_Recv(sub_grid+num_of_pro_size-DIM,DIM,MPI_INT,
+             	next_layer,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	    }else{
 
-        MPI_Gather(sub_grid,1,MPI_INT,updated_grid,1,
-        	MPI_INT,0,MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
+	    	MPI_Recv(sub_grid,DIM,MPI_INT,previous_layer,0,
+	    		MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	    	MPI_Recv(sub_grid+num_of_pro_size-DIM,DIM,MPI_INT,
+	    		next_layer,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 
-	    if(ID==0){
-	    	int p=0;
-	    	for(;p<256;p++){
-	    		global_grid[p] = *(updated_grid+p);
-	    	}
+	    	MPI_Send(sub_grid+DIM,DIM,MPI_INT,previous_layer,0,MPI_COMM_WORLD);
+            MPI_Send(sub_grid+num_of_pro_size-2*DIM,DIM,MPI_INT,next_layer,0,MPI_COMM_WORLD);
 	    }
+        
+	    int* new_sub_grid = get_new_subgrid(sub_grid,num_of_pro_size,DIM);
+        //MPI_Barrier(MPI_COMM_WORLD);
+
+        MPI_Gather(new_sub_grid,num_of_pro_size-2*DIM,MPI_INT,
+        	global_grid,num_of_pro_size-2*DIM,MPI_INT,0,MPI_COMM_WORLD);
 
 	    // Output the updated grid state
 	    if ( ID == 0 ) {
@@ -146,20 +210,16 @@ int main ( int argc, char** argv ) {
 	      printf( "\n" );
 	    }
 
+	    MPI_Barrier(MPI_COMM_WORLD);
+
+	    free(new_sub_grid);
+
         
 	  }
 
 	  // TODO: Clean up memory
 	  free(sub_grid);
-	  if(ID==0){
-	  	free(updated_grid);
-	  }
 	  MPI_Barrier(MPI_COMM_WORLD);
 	  MPI_Finalize(); // finalize so I can exit
 }
 
-void my_bcast(void* data, int count, MPI_Datatype datatype, int root, MPI_Comm communicator){
-
-    
-
-}
